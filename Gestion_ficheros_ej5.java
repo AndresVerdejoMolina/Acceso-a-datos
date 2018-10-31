fd5fe907691ba652b36c45a057985631ad8a7d7d
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-10-31 19:08:32 +0100

    Update Gestion_ficheros_ej5.java

--- Gestion_ficheros_ej5.java
+++ Gestion_ficheros_ej5.java
@@ -3,7 +3,20 @@ package Acceso_a_datos;
 public class Gestion_ficheros_ej5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		File f=null;
+		File[] paths;
+		try {
+			for (String s: args) {
+				f= new File(s);
+				paths = f.listFiles();
+				for(File path:paths) {
+					System.out.println("Nombre del fichero: " + f.getName() + ", Ruta Relativa: " + f.getPath() + ", Ruta abosoluta: " + f.getAbsolutePath() + ", Permiso para escribir: " + f.canWrite() + ", Permiso para leer: " + f.canRead() + ", Tamaño de archivo: " + f.length());
+					
+				}
+			}
+		}catch(NullPointerException e) {
+			System.out.println("No existe");
+		}
 
 	}
 
