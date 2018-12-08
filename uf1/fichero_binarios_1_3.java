22d9d56a4a261c64368ccbaff8d04e68f659c013
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-08 23:40:19 +0100

    Comentarios

--- uf1/fichero_binarios_1_3.java
+++ uf1/fichero_binarios_1_3.java
@@ -6,10 +6,13 @@ public class fichero_binarios_1_3 {
 
 	public static void main(String[] args) throws IOException {
 		DataInputStream dataOS = new DataInputStream (new FileInputStream(new File("C:\\Users\\Usuario\\Desktop\\prueba.txt")));
+		//guardamos el fichero en "dataOS"
 		
 		try {
 			while(true) {
+				//bucle True, para que lea todo el archivo
 				System.out.println("Nombre " + dataOS.readUTF() + " Tiene " + dataOS.readInt() + " años.");
+				//Imprimiendo datos, para leer los Strings utilizamos readUTF, para leer los Int utilizamos el readInt
 			}
 		}catch (EOFException e) {
 			System.out.println("Ya esta!");
