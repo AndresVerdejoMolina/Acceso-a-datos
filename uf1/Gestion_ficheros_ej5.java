1e643b6a1530ce48a67276dc44755af2ad6c1265
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-08 19:34:40 +0100

    Comentarios

--- uf1/Gestion_ficheros_ej5.java
+++ uf1/Gestion_ficheros_ej5.java
@@ -4,17 +4,24 @@ public class Gestion_ficheros_ej5 {
 
 	public static void main(String[] args) {
 		File f=null;
+		//Inicializando "f"
 		File[] paths;
+		//Creando un array de tipo File
 		try {
 			for (String s: args) {
+				//Direccion del fichero guardado en "f"
 				f= new File(s);
 				paths = f.listFiles();
 				for(File path:paths) {
 					System.out.println("Nombre del fichero: " + f.getName() + ", Ruta Relativa: " + f.getPath() + ", Ruta abosoluta: " + f.getAbsolutePath() + ", Permiso para escribir: " + f.canWrite() + ", Permiso para leer: " + f.canRead() + ", Tamaño de archivo: " + f.length());
+					/*Consiguiendo informacion del fichero:
+							Nombre del fichero, ruta relativa, ruta absoluta, en caso de que se pueda escribir devolverá true, en caso de que se pueda escribir devolvera false, en caso de que se pueda leer devolverá true, tamaño del archivo
+							*/
 					
 				}
 			}
 		}catch(NullPointerException e) {
+			//En caso de que no exista el archivo introducido, saltará esta excepción
 			System.out.println("No existe");
 		}
 
