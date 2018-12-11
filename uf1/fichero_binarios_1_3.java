b78cc0a149b8e1e22579baab10948505053ab10e
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 19:11:50 +0100

    fichero binarios

--- uf1/fichero_binarios_1_3.java
+++ uf1/fichero_binarios_1_3.java
@@ -5,6 +5,8 @@ import java.io.*;
 public class fichero_binarios_1_3 {
 
 	public static void main(String[] args) throws IOException {
+		/*Ahora escribe un programa que permita visualizar los datos grabados anteriormente en el fichero FicheroDatos.dat.
+		Se deben obtener en el mismo orden en el que se escribieron, es decir, primero obtenemos el nombre y luego la edad.*/
 		DataInputStream dataOS = new DataInputStream (new FileInputStream(new File("C:\\Users\\Usuario\\Desktop\\prueba.txt")));
 		//guardamos el fichero en "dataOS"
 		
@@ -16,6 +18,8 @@ public class fichero_binarios_1_3 {
 			}
 		}catch (EOFException e) {
 			System.out.println("Ya esta!");
+		} finally {
+			file.close();
 		}
 
 	}
