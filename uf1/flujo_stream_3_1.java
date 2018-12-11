d2073cf77817d3e15fef43e12e0279343d1af64b
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 19:32:55 +0100

    flujo streams

--- uf1/flujo_stream_3_1.java
+++ uf1/flujo_stream_3_1.java
@@ -7,14 +7,16 @@ import java.util.Scanner;
 public class flujo_stream_3_1 {
 
 public static void main(String[]args) throws IOException{
-	System.out.println("introduce la ruta: ");
+	/*Escribe un programa en java que muestre por pantalla un fichero de texto que le pasamos como argumento (o utilizando scanner) utilizando la clase BufferedReader*/	System.out.println("introduce la ruta: ");
 	Scanner scan = new Scanner(System.in);
 	String linea = scan.nextLine();
     FileReader in = new FileReader(linea);
+	//guardando la entrada en una variable tipo FileReader para leer
     BufferedReader br = new BufferedReader(in);
 
     String line;
     while ((line = br.readLine()) != null) {
+	    //Imprimiendo linea a linea
         System.out.println(line);
     }
     in.close();
