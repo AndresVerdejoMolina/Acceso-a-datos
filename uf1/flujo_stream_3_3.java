9af9cb98b01bd7eb3f3d7942a43d0809ab002a01
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 19:38:34 +0100

    flujo streams

--- uf1/flujo_stream_3_3.java
+++ uf1/flujo_stream_3_3.java
@@ -4,11 +4,12 @@ import java.io.*;
 public class flujo_strems_3 {
 
 	public static void main(String[] args)throws IOException {
+		/*Repite el ejercicio anterior pero ahora utilizando la clase PrintWriter*/
 		PrintWriter pw = new PrintWriter(new FileWriter("C:\\Users\\Usuario\\Desktop\\prueba.txt"));
-		 
-		for (int i = 0; i < 10; i++) {
-			
-			pw.println("something");
+		//Con PrintWritter, en una linea declaramos que vamos a escribir en ese archivo		 
+		for (int i = 0; i < 10; i++) {			
+			pw.println("informatica");
+			//Escribimos 10 veces "informatica"
 		}
 	 
 		pw.close();
