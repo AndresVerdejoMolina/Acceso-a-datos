5f579bbe4ca2a4cce0dd1d648c77101926bb123a
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 18:55:01 +0100

    AccesoSAX

--- uf1/accesos a fichero con SAX_2.java
+++ uf1/accesos a fichero con SAX_2.java
@@ -1,10 +1,26 @@
+import java.io.*;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 public class acceso_sax {
 
+/*Prueba a leer con el programa del ejercicio anterior el documento 
+“personas.xml” (se trata del xml que generamos en la clase anterior)*/
+
 	public static void main(String[] args)	throws FileNotFoundException, IOException, SAXException {
+		/* A continuación se crea objeto procesador XML - XMLReader -. Durante la creación de este objeto se puede producir una
+		excepción SAXException. */
 			XMLReader procesadorXML = XMLReaderFactory.createXMLReader();
+		/* A continuación, mediante setContentHandler establecemos que la clase que gestiona los eventos provocados por la
+		lectura del XML será GestionContenido */
 			GestionContenido gestor = new GestionContenido();
 			procesadorXML.setContentHandler(gestor);
+		/* Por último, se define el fichero que se va leer mediante InputSource y se procesa el documento XML mediante el
+		método parse() de XMLReader */
 			InputSource fileXML = new InputSource ("C:\\Users\\Usuario\\Desktop\\DAM2\\discoteca.xml");
 			procesadorXML.parse(fileXML);
 			}
