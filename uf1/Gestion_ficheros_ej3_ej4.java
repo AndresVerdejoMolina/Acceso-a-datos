583ccd8c46afb769faa6652c09d2525401fcfdb2
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 18:43:06 +0100

    Gestion ficheros

--- uf1/Gestion_ficheros_ej3_ej4.java
+++ uf1/Gestion_ficheros_ej3_ej4.java
@@ -2,16 +2,19 @@ package Acceso_a_datos;
 import java.io.File;
 
 public class Gestion_ficheros_ej3_ej4 {
+	/*Añade al programa anterior las instrucciones necesarias para que envíe un mensaje de error en caso de que el directorio pasado como argumento no 
+exista.
+*/
 
 	public static void main(String[] args) {
 		//Archivo entrado como argumento
-		File f=null;
+		File a=null;
 		File[] paths;
 		//Creando una array de tipo fichero
 		try {
 			for (String s: args) {
-				f= new File(s);
-				paths = f.listFiles();
+				a= new File(s);
+				paths = a.listFiles();
 				//Mostrar todos los archivos del directorio y guardarlos en "paths"
 				for(File path:paths) {
 					System.out.println(path);
