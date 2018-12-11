803b24c512b6a0b25af4fd2e7cf14d00e4584e4b
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 18:57:28 +0100

    AccesoSAX

--- uf1/accesos a fichero con SAX_3.java
+++ uf1/accesos a fichero con SAX_3.java
@@ -3,11 +3,20 @@ import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
 
 public class acceso_sax {
+/*Crea un programa que lea el archivo “discoteca.xml” (adjunto en el moodle) y 
+determine el número de discos listados en el archivo. AYUDA: contabilizando 
+el número de títulos.*/
 
 	public static void main(String[] args)	throws FileNotFoundException, IOException, SAXException {
+		/* A continuación se crea objeto procesador XML - XMLReader -. Durante la creación de este objeto se puede producir una
+		excepción SAXException. */
 			XMLReader procesadorXML = XMLReaderFactory.createXMLReader();
+		/* A continuación, mediante setContentHandler establecemos que la clase que gestiona los eventos provocados por la
+		lectura del XML será GestionContenido */
 			GestionContenido gestor = new GestionContenido();
 			procesadorXML.setContentHandler(gestor);
+		/* Por último, se define el fichero que se va leer mediante InputSource y se procesa el documento XML mediante el
+		método parse() de XMLReader */
 			InputSource fileXML = new InputSource ("C:\\Users\\Usuario\\Desktop\\DAM2\\discoteca.xml");
 			procesadorXML.parse(fileXML);
 			}
@@ -19,7 +28,7 @@ class GestionContenido extends DefaultHandler {
 			super();
 		}
 		public void endDocument(){
-		//imprimiendo variable contador
+		//imprimiendo variable contador cuando llegue al final del fichero
 			System.out.println(contador);
 		}
 		public void startElement (String uri, String nombre, String nombreC, Attributes atts) {
