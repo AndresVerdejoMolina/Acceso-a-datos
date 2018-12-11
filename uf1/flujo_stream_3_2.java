fc8e75f091dee6ff8d51adcd4ebd0d9c8e6ead65
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 19:36:53 +0100

    flujo streams

--- uf1/flujo_stream_3_2.java
+++ uf1/flujo_stream_3_2.java
@@ -4,14 +4,16 @@ import java.io.*;
 public class flujo_strems_3 {
 
 	public static void main(String[] args)throws IOException {
+		/*Escribe un programa en java que muestre por pantalla un fichero de texto que le pasamos como argumento (o utilizando scanner) utilizando la clase BufferedReader*/
 		File fout = new File("C:\\Users\\Usuario\\Desktop\\prueba.txt");
-		FileOutputStream fos = new FileOutputStream(fout);
-	 
+		FileOutputStream fos = new FileOutputStream(fout);	 
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+		//Guardamos la direccion del fichero en un BufferedWritter
 	 
 		for (int i = 0; i < 10; i++) {
 			bw.write("Hola");
 			bw.newLine();
+			//Escibimos 10 veces "Hola", y con {bw.newLine()} saltamos una linea
 		}
 	 
 		bw.close();
