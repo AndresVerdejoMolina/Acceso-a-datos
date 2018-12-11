658f95ea432fbb227f6fa4d17c70f4f00d53ccf4
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 18:23:23 +0100

    Gestión de ficheros

--- uf1/Gestion_ficheros_ej1.java
+++ uf1/Gestion_ficheros_ej1.java
@@ -2,12 +2,15 @@ package Acceso_a_datos;
 import java.io.*;
 
 public class Gestion_ficheros_ej1 {
-
+	/*Realiza un programa Java que utilice el método listFiles() para mostrar la lista 
+	de ficheros de un directorio que se pasará al programa desde los argumentos del	main
+	*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String dir="\\C:\\Users\\Usuario\\Downloads";
 		//Direccion del directorio
 		File f=new File(dir);
+		//Creand variable de tipo archivo donde guardaremos la direccion
 		String[] archivos=f.list();
 		System.out.printf("Fichero en el directorio actual: %d %n", archivos.length);
 		//Numero de objetos que compone el directorio
