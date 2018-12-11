723aa9d49d400a3b326527487413f211a33db564
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 19:01:50 +0100

    AccesoSAX

--- uf1/accesos a fichero con SAX_4.java
+++ uf1/accesos a fichero con SAX_4.java
@@ -6,15 +6,26 @@ import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
 
 public class acceso_sax_4 {
+/*Crea un programa que devuelva el número de discos registrados en 
+“discoteca.xml” de un determinado autor que le pasamos por consola. Si el 
+autor carece de discos en el archivo, el programa devolverá un mensaje del 
+estilo: “El autor <xxxxxx> no aparece en el archivo.*/
+
 		public static void main(String[] args)	throws FileNotFoundException, IOException, SAXException {
+		/* A continuación se crea objeto procesador XML - XMLReader -. Durante la creación de este objeto se puede producir una
+		excepción SAXException. */
 				XMLReader procesadorXML = XMLReaderFactory.createXMLReader();
 				System.out.println("Introduce un artista:");
 				Scanner primero= new Scanner(System.in);
 				String artista=primero.nextLine();
 				//Obteniendo nombre del artista
+				/* A continuación, mediante setContentHandler establecemos que la clase que gestiona los eventos provocados por la
+			lectura del XML será GestionContenido */
 				GestionContenido gestor = new GestionContenido(artista);
 				//inicializando variable "gestor" de tipo GestionContenido
 				procesadorXML.setContentHandler(gestor);
+				/* Por último, se define el fichero que se va leer mediante InputSource y se procesa el documento XML mediante el
+		método parse() de XMLReader */
 				InputSource fileXML = new InputSource ("C:\\Users\\Usuario\\Desktop\\DAM2\\discoteca.xml");
 				procesadorXML.parse(fileXML);
 				}
@@ -27,10 +38,7 @@ public class acceso_sax_4 {
 				this.artista=Artista;				
 			}
 			public void endDocument(){
-				System.out.println(contador);
-			}
-			public void startElement (String uri, String nombre, String nombreC, Attributes atts) {
-			
+				System.out.println("Hay :" + contador + " artistas");
 			}
 			public void characters(char[] ch, int inicio, int longitud) throws SAXException {
 				String car = new String (ch, inicio, longitud);
