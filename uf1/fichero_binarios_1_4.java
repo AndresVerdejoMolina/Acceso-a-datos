cddf64e7cda819b3af344948c027fff7fea83f15
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 19:12:47 +0100

    fichero binarios

--- uf1/fichero_binarios_1_4.java
+++ uf1/fichero_binarios_1_4.java
@@ -1,4 +1,4 @@
 Vuelve a ejecutar los problemas anteriores pero en los pasos intermedios 
 trata de leer el fichero “FicheroDatos.dat” con algún editor de texto del 
 sistema. ¿Qué pasa? 
-Se muestra los nombre, osea los Strings, pero las edades no me las enseña.
+Se muestra solo los Strings, pero los ints, como las edades, no las muestra.
