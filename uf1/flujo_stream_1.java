5d82eacec0b57a9bdb915d5f0e65cdb4f364ac93
AndresAlejandro <alejandro.verdejo@insjoandaustria.org>
2018-12-11 19:21:19 +0100

    flujo streams

--- uf1/flujo_stream_1.java
+++ uf1/flujo_stream_1.java
@@ -6,10 +6,7 @@ public class flujo_strems_1 {
 
 	public static void main(String[] args)throws IOException  {
 		/*Modifica el código anterior para que el programa vaya leyendo caracteres de 20 en 20.*/
-			System.out.println("introuduce la ruta");
-			Scanner leer= new Scanner(System.in);
-			String input= leer.nextLine();
-			File fichero = new File (input);  // declaración fichero
+			File fichero = new File ("data.txt");  // declaración fichero
 			FileReader flu = new FileReader (fichero); // creamos flujo de entrada hacia el fichero		
 			char[] buf = new char[20];
 			while ((flu.read(buf))!=-1) {    //Vamos leyendo carácter a carácter
